19202918370df20262634cb424066a542a918092
Jaakko Keränen <jaakko.keranen@iki.fi>
2019-12-02 20:01:20 +0200

    Linux: Added a link to the unstable Flatpak repository

--- web2/linux.php
+++ web2/linux.php
@@ -4,8 +4,8 @@ $page_title = 'Linux Downloads';
 generate_page_header($page_title);
 ?>
 <body>
-    <?php 
-    include(DENG_LIB_DIR.'/topbar.inc.php'); 
+    <?php
+    include(DENG_LIB_DIR.'/topbar.inc.php');
     generate_page_title($page_title);
     ?>
     <div id='content'>
@@ -15,7 +15,7 @@ generate_page_header($page_title);
     <article>
         <h1>Stable</h1>
         <p>This is the latest stable version. <?php echo(release_notes_link('ubuntu18-x86_64')); ?></p>
-        <?php 
+        <?php
         generate_badges('ubuntu18-x86_64', BT_CANDIDATE);
         generate_badges('fedora23-x86_64', BT_CANDIDATE);
         ?>
@@ -25,7 +25,7 @@ generate_page_header($page_title);
     <article>
         <h1>Unstable</h1>
         <p>Unstable builds are made automatically every day when changes are committed to the <a href="source">source repository</a>. They contain work-in-progress code and sometimes may crash on you. Change logs can be found in the <a href="/builds">Autobuilder</a>.</p>
-        <?php 
+        <?php
         generate_badges('ubuntu18-x86_64', BT_UNSTABLE);
         generate_badges('fedora23-x86_64', BT_UNSTABLE);
         ?>
@@ -35,6 +35,9 @@ generate_page_header($page_title);
     <article>
         <h1>Other Builds</h1>
         <p>Binary packages are available for some Linux distributions. You can also <a href="/manual/devel/compile">compile manually from source</a> (requires CMake 3.1 and Qt 5).</p>
+
+        <h2>Flatpak</h2>
+        <p><a href="https://flatpak.org">Flatpak</a> allows building and distributing desktop applications on many Linux distros. There is a <a href="http://files.dengine.net/doomsday-unstable.flatpakref">Flakpak repository for unstable builds (files.dengine.net/repo/)</a>.</p>
 
         <h2><img src="/images/ubuntu.png" alt="Ubuntu" class="distro-icon">Ubuntu</h2>
         <p><a class="link-external" href="https://launchpad.net/~sjke/+archive/ubuntu/doomsday/">skyjake's PPA</a> has Doomsday builds for a number of versions of Ubuntu. The <a class="link-external" href="http://packages.ubuntu.com/xenial/games/doomsday">Ubuntu repositories</a> should also have stable releases of Doomsday.</p>
