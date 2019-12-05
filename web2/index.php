5712246216c5ef1c70c5c957f8e06cb59f4d0ca8
Jaakko Keränen <jaakko.keranen@iki.fi>
2019-12-05 14:36:57 +0200

    Alternative front page welcome messages

--- web2/index.php
+++ web2/index.php
@@ -1,6 +1,18 @@
 <?php
 require_once('include/template.inc.php');
 generate_page_header();
+
+$welcome_msgs = [
+    "WELCOME BACK TO THE 1990s",
+    "486DX2 66MHZ TURBO",
+    "MONSTERS IN THE DARK",
+    "FIRE UP THE CHAINSAW",
+    "KNEE-DEEP IN THE DEAD",
+    "THE CYBERDEMON AWAITS",
+    "THE SERPENT RIDERS AWAIT",
+    "FIGHTER, MAGE, OR CLERIC?"
+];
+
 ?>
 <body>
     <?php include(DENG_LIB_DIR.'/topbar.inc.php'); ?>
@@ -9,13 +21,15 @@ generate_page_header();
             <div class='main-logo'></div>
             <div class='intro'>
                 <section>
-                    <h1>WELCOME BACK TO THE 1990s</h1>
+                    <h1><?php
+                        echo($welcome_msgs[random_int(0, count($welcome_msgs) - 1)]);
+                    ?></h1>
                     <p>Doomsday Engine is a Doom / Heretic / Hexen port with enhanced graphics</p>
                     <div class="download-button">
                         <?php generate_download_button(); ?>
                     </div>
                 </section>
-            </div>            
+            </div>
         </div>
         <div id='hero' class='block'>
             <article>
@@ -26,16 +40,16 @@ generate_page_header();
                     <img src='theme/images/demo-thumb.jpg'>
                 </video>
                 <h1>A rejuvenation.</h1>
-                <p><a href="https://en.wikipedia.org/wiki/Doom_(1993_video_game)">id 
+                <p><a href="https://en.wikipedia.org/wiki/Doom_(1993_video_game)">id
                     Software's Doom</a> pioneered the modern first-person shooter genre.
                     Released in 1993, it was a quantum leap in game engine technology
-                    with fluid and &mdash; at the time &mdash; incredibly realistic 
+                    with fluid and &mdash; at the time &mdash; incredibly realistic
                     3D graphics.</p>
-                <p>While you can still enjoy the original Doom and 
+                <p>While you can still enjoy the original Doom and
                     <a href="https://en.wikipedia.org/wiki/First-person_shooter#Rise_in_popularity:_1992.E2.80.931995">its progeny</a>
-                    today in an emulator, modern games are held to higher standards of 
-                    visual fidelity, usability, and multiplayer features. Doomsday 
-                    Engine exists to refresh the technology of these classic games 
+                    today in an emulator, modern games are held to higher standards of
+                    visual fidelity, usability, and multiplayer features. Doomsday
+                    Engine exists to refresh the technology of these classic games
                     while retaining the core gameplay experience.
                 </p>
             </article>
